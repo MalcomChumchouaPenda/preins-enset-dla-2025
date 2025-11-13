dac65eb81dcdd887bc881d442ab8244607f0da08
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2025-11-13 11:31:02 +0100

    debug CI/CD

--- app.py
+++ app.py
@@ -31,27 +31,9 @@ def change_lang():
     return redirect(next)
 
 
-def __is_valid_signature(x_hub_signature, data, private_key):
-    # x_hub_signature and data are from the webhook payload
-    # private key is your webhook secret
-    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
-    algorithm = hashlib.__dict__.get(hash_algorithm)
-    encoded_key = bytes(private_key, 'latin-1')
-    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
-    return hmac.compare_digest(mac.hexdigest(), github_signature)
-
-
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        w_secret = os.environ.get('GIT_SECRET_KEY', '')
-        if not w_secret:
-            return "Unauthorized access", 401
-        
-        x_hub_signature = request.headers.get('X-Hub-Signature')
-        if not __is_valid_signature(x_hub_signature, request.data, w_secret):
-            return "Unauthorized access", 401
-        
         repo = git.Repo(ROOT_DIR)
         origin = repo.remotes.origin
         origin.pull()
