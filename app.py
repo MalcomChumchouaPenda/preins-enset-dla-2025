0b8967f3a91017053c71ff64a973c539f4813ee7
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2025-11-13 11:15:19 +0100

    secure CI-CD

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ from flask import redirect, url_for, request, session
 from core.utils import read_markdown, ROOT_DIR
 from core.config import create_app
 # from flask_mail import Mail
+
 import git
+import hmac
+import hashlib
+
 
 app = create_app()
 
@@ -26,9 +30,23 @@ def change_lang():
     session['lang'] = lang
     return redirect(next)
 
+
+def __is_valid_signature(x_hub_signature, data, private_key):
+    # x_hub_signature and data are from the webhook payload
+    # private key is your webhook secret
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
+
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
+        w_secret = os.environ.get('GIT_SECRET_KEY')
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        if not __is_valid_signature(x_hub_signature, request.data, w_secret):
+            return "Unauthorized access", 401
         repo = git.Repo(ROOT_DIR)
         origin = repo.remotes.origin
         origin.pull()
