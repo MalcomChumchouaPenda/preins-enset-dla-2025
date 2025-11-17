b24bcd9aa5705b2afd51d3a15c6d6ebace9c65cd
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2025-11-17 07:33:11 +0100

    debug user creation

--- services/quitus_v0_0/defaults.py
+++ services/quitus_v0_0/defaults.py
@@ -2,7 +2,7 @@
 import os
 import csv
 from core.config import db
-from core.auth.tasks import add_role, add_user, add_roles_to_user
+from core.auth.tasks import add_role, add_user, add_roles_to_user, get_user
 
 
 store_dir = os.path.join(os.path.dirname(__file__), 'store')
@@ -17,7 +17,8 @@ def _read_csv(filename, sep=','):
 
 def init_data():
     session = db.session
-    add_user(session, 'dev0', 'Root developper', '2025')
+    if not get_user(session, 'dev0'):
+        add_user(session, 'dev0', 'Root developper', '2025')
     add_role(session, 'admin_quitus', 'Gestionnaire Quitus')
     add_roles_to_user(session, 'dev0', 'admin_quitus', 'developper', 'admin_preins')
 
