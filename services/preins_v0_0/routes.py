4d154a6f198f4ff1f645d2aae1588de95df5b796
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2026-01-08 06:51:03 +0100

    add public api for quitus and admissions

--- services/preins_v0_0/routes.py
+++ services/preins_v0_0/routes.py
@@ -1,6 +1,29 @@
 from flask import request, abort
 from flask_restx import Resource, fields
 from core.utils import ApiNamespace
+from .models import Admission
 
 
 ns = ApiNamespace('preins', description="Gestion des inscriptions")
+
+
+@ns.route('/admissions')
+# @ns.roles_accepted('admin_preins')
+class AdmissionsApi(Resource):
+
+    def get(self):
+        query = Admission.query
+        return [
+            { 
+               "id":row.id,
+               "matricule":row.matricule,
+               "classe_id":row.classe_id,
+               "niveau":row.classe.niveau_id,
+               "communique_id":row.communique_id,
+               "filiere":row.classe.filiere.nom,
+               "departement_id":row.classe.filiere.departement_id,
+               "departement":row.classe.filiere.departement.nom,
+            } 
+            for row in query.all()]
+    
+
--- services/quitus_v0_0/routes.py
+++ services/quitus_v0_0/routes.py
@@ -1,4 +1,26 @@
 
+from flask_restx import Resource, fields
 from core.utils import ApiNamespace
+from .models import Quitus
+
 
 ns = ApiNamespace('quitus', description="Gestion des quitus de paiement")
+
+
+quitus_model = ns.model('quitus_v0', {
+    'id': fields.String,
+    'matricule': fields.String,
+    'classe_id': fields.String,
+    'communique_id': fields.String
+})
+
+
+@ns.route('/quitus')
+# @ns.roles_accepted('admin_preins')
+class QuitusApi(Resource):
+
+    @ns.marshal_list_with(quitus_model)
+    def get(self):
+        query = Quitus.query
+        return query.all()
+    
